57814af99fbda86ad07465cb664e209107d24971
“nhankhtn” <nguyenanhnguyen3006@gmail.com>
2024-09-23 16:53:06 +0700

    update: add middleware cors

--- backend/src/routes/site.js
+++ backend/src/routes/site.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
 
-router.get("/", (req, res) => { return res.send("Hello world!") });
+router.get("/", (req, res) => { return res.json({ message: "Hello world!!!" }) });
 
 module.exports = router;
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
 const express = require("express");
+const cors = require("cors");
 const morgan = require("morgan");
 
 const route = require("./routes")
@@ -9,6 +10,9 @@ const PORT = process.env.PORT || 8000;
 
 // Apply middleware
 app.use(morgan("dev"))
+app.use(cors({
+    origin: process.env.URL_FE,
+}))
 
 route(app);
 

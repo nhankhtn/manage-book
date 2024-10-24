9c7a54d354778d70d61a453edd383b8813f4de17
“nhankhtn” <nguyenanhnguyen3006@gmail.com>
2024-10-24 23:32:31 +0700

    fe: change call route api

--- frontend/src/services/searchService.js
+++ frontend/src/services/searchService.js
@@ -2,7 +2,7 @@ import * as httpRequest from '@/utils/httpRequest';
 
 export const searchBooks = async (params) => {
     try {
-        const response = await httpRequest.get('/books/search', { params });
+        const response = await httpRequest.get('/books', { params });
         return response;
     } catch (error) {
         throw error;

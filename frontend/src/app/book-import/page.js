475c1317e5033cd58f8b167f3135e633ee9d5c5d
Minh Nhat <ndminhnhat1234@gmail.com>
2024-10-27 19:44:28 +0700

    #1 handle err and modal alert

--- frontend/src/app/book-import/page.js
+++ frontend/src/app/book-import/page.js
@@ -8,11 +8,18 @@ import Table from "@/components/Table";
 import Button from "@/components/Button";
 import Modal from "@/components/Modal";
 import FormAddBook from "@/components/FormAddBook/FormAddBook";
+import ModalAlert from "@/components/ModalAlert";
 export default function BookImport() {
     const [showModalAdd, setShowModalAdd] = useState(false);
+    const [showModalAlert, setShowModalAlert] = useState({
+        show: false,
+        success: false,
+    });
+    // err khi chưa thêm sách
+    const [err, setErr] = useState(false);
     const [books, setBooks] = useState([
     ]);
-
+    const [loading, setLoading] = useState(false);
     function deleteAt(index) {
         setBooks(preValues => {
             return preValues.filter((value, i) => i !== index);
@@ -21,13 +28,45 @@ export default function BookImport() {
     }
 
     async function handleImportBook() {
-        const result = await updateBooks(books);
+     
+        if(books.length === 0) {
+            setErr(true);
+            return;
+        }
+        try{
+            setLoading(true);
+            const result = await updateBooks(books);
+            if(result.message === "Lỗi khi cập nhật sách") {
+                setShowModalAlert({
+                    show: true,
+                    success: false,
+                });
+            }
+            else{
+                setShowModalAlert({
+                    show: true,
+                    success: true,
+                });
+                setBooks([]);
+            }
+        }
+        catch(error) {
+            setShowModalAlert({
+                show: true,
+                success: false,
+            });
+        }
+        finally {
+            setLoading(false);
+        }
+     
     }
     function handleAdd(book) {
         setBooks(preValues => {
             return [...preValues, book];
         });
         setShowModalAdd(false);
+        setErr(false);
     }
     return (
         <>
@@ -40,13 +79,13 @@ export default function BookImport() {
                     <Button onClick={() => setShowModalAdd(true)} >Thêm sách</Button>
                 </div>
                 <Table fieldCols={BOOK_FIELDS} data={books} deleteRow={deleteAt} placeholder="Ngày nhập sách" />
-                <Button onClick={handleImportBook} style={{
-                    marginTop: "40px",
-                }} >Hoàn tất</Button>
+                <Button onClick={handleImportBook} className={styles.import_btn} disabled={loading} >{loading ? "Loading..." : "Hoàn tất"}</Button>    
+                {err && <div className={styles.error}>Vui lòng thêm sách</div>}
             </div>
             <Modal show={showModalAdd} onHide={() => setShowModalAdd(false)}>
                 <FormAddBook handleAdd={handleAdd} />
             </Modal>
+            <ModalAlert show={showModalAlert.show} success={showModalAlert.success} onHide={() => setShowModalAlert(false)} />
         </>
     )
 }
--- frontend/src/components/FormAddBook/FormAddBook.js
+++ frontend/src/components/FormAddBook/FormAddBook.js
@@ -6,9 +6,16 @@ export default function FormAddBook( {handleAdd} ) {
     title: "",
     author: "",
     category: "",
-    quantity: 0,
-    price: 0,
+    quantity: null,
+    price: null,
   });
+  const [errors, setErrors] = useState({
+    title: '',
+    author: '',
+    category: '',
+    quantity: '',
+    price: ''
+});
   function handleChange(e) {
     const { id, value } = e.target;
     setFormData((preValues) => {
@@ -20,12 +27,45 @@ export default function FormAddBook( {handleAdd} ) {
   }
   function handleSubmit(e) {
     e.preventDefault();
-    if (!formData.title || !formData.author || !formData.category) {
-      alert("Please fill all fields");
-      return;
+    // if (!formData.title || !formData.author || !formData.category) {
+    //   alert("Please fill all fields");
+    //   return;
+    // }
+    // if(formData.quantity <= 0 || formData.price <= 0) {
+    //   alert("Quantity and price must be greater than 0");
+    //   return;
+    // }
+    // Validate input and set error messages
+    let validationErrors = {
+      title: '',
+      author: '',
+      category: '',
+      quantity: '',
+      price: ''
+    };
+    let hasErr= false;
+    if (formData.title.trim() === '') {
+        validationErrors.title = 'Title is required';
+        hasErr= true;
     }
-    if(formData.quantity <= 0 || formData.price <= 0) {
-      alert("Quantity and price must be greater than 0");
+    else if (formData.author.trim() === '') {
+        validationErrors.author = 'Author is required';
+        hasErr= true;
+    }
+    else if (formData.category.trim() === '') {
+        validationErrors.category = 'Category is required';
+        hasErr= true;
+    }
+    else if (formData.quantity <= 0) {
+        validationErrors.quantity = 'Quantity must be greater than 0';
+        hasErr= true;
+    }
+    else if (formData.price <= 0) {
+        validationErrors.price = 'Price must be greater than 0';
+        hasErr= true;
+    }
+    if(hasErr) {
+      setErrors(validationErrors);
       return;
     }
     const book = formData;
@@ -35,25 +75,30 @@ export default function FormAddBook( {handleAdd} ) {
     <div className={styles.container}>
       <div className={styles.title} >Add Book</div>
       <form onSubmit={handleSubmit}>
-        <div>
+        <div className={!errors.title ? styles.errorGap : ""}>
           <label htmlFor="title" >Tên sách</label>
           <input id="title" type="text" onChange={handleChange} />
+          {errors.title && <div className={styles.error}>{errors.title}</div>}
         </div>
-        <div>
+        <div className={!errors.author ? styles.errorGap : ""}>
           <label htmlFor="author">Tác giả</label>
           <input id="author" type="text" onChange={handleChange} />
+          {errors.author && <div className={styles.error}>{errors.author}</div>}
         </div>
-        <div>
+        <div className={!errors.category ? styles.errorGap : ""}>
           <label htmlFor="category">Thể loại</label>
           <input id="category" type="text" onChange={handleChange} />
+          {errors.category && <div className={styles.error}>{errors.category}</div>}
         </div>
-        <div>
+        <div  className={!errors.quantity ? styles.errorGap : ""}>
           <label htmlFor="quantity">Số lượng</label>
-          <input id="quantity" type="number" onChange={handleChange} defaultValue={0} />
+          <input id="quantity" type="number" onChange={handleChange} placeholder="0" />
+          {errors.quantity && <div className={styles.error}>{errors.quantity}</div>}
         </div>
-        <div>
+        <div  className={!errors.price ? styles.errorGap : ""}>
           <label htmlFor="price" >Giá</label>
-          <input id="price" type="number" onChange={handleChange} defaultValue={0} />
+          <input id="price" type="number" onChange={handleChange}  placeholder="0" />
+          {errors.price && <div className={styles.error}>{errors.price}</div>}
         </div>
         <Button className={styles.submitBtn} type="submit">Submit</Button>
       </form>

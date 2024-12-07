3c427b4892cc6df3c2f80210f7514710d948af87
22120255 <22120255@student.hcmus.edu.vn>
2024-12-07 23:22:20 +0700

    nothing change

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,21 +3,22 @@ const express = require("express");
 const cors = require("cors");
 const morgan = require("morgan");
 
-const route = require("./routes")
+const route = require("./routes");
 
 const app = express();
 const PORT = process.env.PORT || 8000;
 
-
 // Apply middleware
 app.use(express.json());
-app.use(morgan("dev"))
-app.use(cors({
+app.use(morgan("dev"));
+app.use(
+  cors({
     origin: process.env.URL_FE,
-}))
+  })
+);
 
 route(app);
 
 app.listen(PORT, () => {
-    console.log(`Server is running on localhost:${PORT}`);
-})
+  console.log(`Server is running on localhost:${PORT}`);
+});

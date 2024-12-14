fb8f7f86ab72d3101dbc78bf25677c455c6cbf6d
nhankhtn <nguyenanhnguyen3006@gmail.com>
2024-12-14 18:05:46 +0700

    update: test feature login & import book

--- frontend/cypress/e2e/book-import.cy.js
+++ frontend/cypress/e2e/book-import.cy.js
@@ -8,13 +8,28 @@ describe('Book Import Test', () => {
 
             cy.url().should('include', '/dashboard/book-import');
 
-            // Giả lập nhập liệu vào form và gửi
-            cy.get('input[name="bookTitle"]').type('New Book Title');
-            cy.get('input[name="author"]').type('Book Author');
-            cy.get('button[type="submit"]').click();
+            cy.get('button').contains('Thêm sách').click();
 
-            // Kiểm tra thông báo thành công
-            cy.get('.alert-success').should('be.visible');
+            const minStockQuantityBeforeImport = 40;
+
+            cy.fixture('books').then((books) => {
+                books.forEach(book => {
+                    // Điền thông tin sách vào form thêm sách
+                    cy.get('input#title').type(book.title);
+                    cy.get('input#author').type(book.author);
+                    cy.get('input#category').type(book.category);
+                    cy.get('input#quantity').type(book.quantity);
+                    cy.get('input#price').type(book.price);
+                    // Thực hiện submit form thêm sách
+                    cy.get('#btn-form-add-book').click();
+
+                    if (parseInt(book.quantity) < minStockQuantityBeforeImport) {
+                        cy.get('.error').should('be.visible').and('contain', `Số lượng sách phải từ ${minStockQuantityBeforeImport} trở lên`);
+                    } else {
+                        cy.get('table').contains(book.title);
+                    }
+                });
+            })
         });
     });
 });
